f68e8aa20cc05369a6a8a8eef7b8da4e17dd538c
bao <thobaocss@gmail.com>
2018-06-13 15:40:00 +0700

    update text news

--- js/promotion.module.js
+++ js/promotion.module.js
@@ -506,26 +506,6 @@ function getListPromotionById(id, isView){
             promotion_id: id
         },
         success: function (result){
-            tinymce.init({
-// General options
-                selector: "textarea#promotionDetail",
-                mode : "specific_textareas",
-                theme : "advanced",
-                width: "100%",
-                plugins : "pagebreak,paste,fullscreen,visualchars",
-
-// Theme options
-                theme_advanced_buttons1 : "code,|,bold,italic,underline,|,sub,sup,|,charmap,|,fullscreen,|,bullist,numlist,|,pasteword",
-                theme_advanced_buttons2 :"",
-                theme_advanced_buttons3 :"",
-                theme_advanced_buttons4 :"",
-                theme_advanced_toolbar_location : "top",
-                theme_advanced_toolbar_align : "left",
-                theme_advanced_statusbar_location : "bottom",
-                valid_elements : "i,sub,sup",
-                invalid_elements : "p, script",
-                editor_deselector : "mceOthers"
-            });
             result = $.parseJSON(result);
             $('#modalForm').modal('show');
             var promotionData = result.data.result;
