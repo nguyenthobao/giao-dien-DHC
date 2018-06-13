abb2c99ea5b049bf36ac54e416941352d36af88c
bao <thobaocss@gmail.com>
2018-06-13 15:43:48 +0700

    update text news

--- js/promotion.module.js
+++ js/promotion.module.js
@@ -506,6 +506,7 @@ function getListPromotionById(id, isView){
             promotion_id: id
         },
         success: function (result){
+            tinymce.init({ selector:'textarea#promotionDetail' });
             result = $.parseJSON(result);
             $('#modalForm').modal('show');
             var promotionData = result.data.result;
